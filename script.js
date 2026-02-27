0ada642e2a1db66e5c189a64a8124ffa7c5bc846
KrisvHeij <49883345+KrisvHeij@users.noreply.github.com>
2026-02-27 21:43:03 +0100

    Updated

---
+++ script.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("menu-button");
+const menu = document.getElementById("menu");
+
+btn.addEventListener("click", ()=> {
+  menu.style.transform = "translateX(0%)";
+})
