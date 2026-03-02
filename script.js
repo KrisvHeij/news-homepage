8fa6d78d4af29aa13654f2bbc757df309246b1e4
KrisvHeij <49883345+KrisvHeij@users.noreply.github.com>
2026-03-02 22:31:29 +0100

    Updated

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const btn = document.getElementById("menu-button");
 const menu = document.getElementById("menu");
+const menuImg = document.querySelector("#menu-button img");
 
+// Open & close menu
 btn.addEventListener("click", ()=> {
-  menu.style.transform = "translateX(0%)";
+  menu.classList.toggle("menu-show");
+  
+  if (menu.classList.contains("menu-show")) {
+    btn.setAttribute("aria-expanded", "true");
+    menuImg.src = "./assets/images/icon-menu-close.svg";
+  } else {
+    btn.setAttribute("aria-expanded", "false");
+    menuImg.src = "./assets/images/icon-menu.svg";
+  }
 })
