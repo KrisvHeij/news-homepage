fac1b51fb7a3d0b5f5b59bff8ede5e30fdbacad8
KrisvHeij <49883345+KrisvHeij@users.noreply.github.com>
2026-03-03 20:11:44 +0100

    Updated

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const btn = document.getElementById("menu-button");
 const menu = document.getElementById("menu");
 const menuImg = document.querySelector("#menu-button img");
+const darkBg = document.querySelector(".bg-darken");
 
 // Open & close menu
 btn.addEventListener("click", ()=> {
   menu.classList.toggle("menu-show");
+  darkBg.classList.toggle("visually-hidden");
   
   if (menu.classList.contains("menu-show")) {
     btn.setAttribute("aria-expanded", "true");
