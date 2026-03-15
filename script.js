f7a8cd6ba61fa8392735d91d4fe40df8c2647dde
KrisvHeij <49883345+KrisvHeij@users.noreply.github.com>
2026-03-15 16:06:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-const btn = document.getElementById("menu-button");
-const menu = document.getElementById("menu");
-const menuImg = document.querySelector("#menu-button img");
-const darkBg = document.querySelector(".bg-darken");
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("menu-button");
+  const menu = document.getElementById("menu");
+  const menuImg = document.querySelector("#menu-button img");
+  const darkBg = document.querySelector(".bg-darken");
 
-// Open & close menu
-btn.addEventListener("click", ()=> {
-  menu.classList.toggle("menu-show");
-  darkBg.classList.toggle("visually-hidden");
-  
-  if (menu.classList.contains("menu-show")) {
-    btn.setAttribute("aria-expanded", "true");
-    menuImg.src = "./assets/images/icon-menu-close.svg";
-  } else {
-    btn.setAttribute("aria-expanded", "false");
-    menuImg.src = "./assets/images/icon-menu.svg";
+  if (!btn || !menu) {
+    return;
   }
+
+    // Open & close menu
+  btn.addEventListener("click", ()=> {
+    menu.classList.toggle("menu-show");
+    darkBg.classList.toggle("visually-hidden");
+    
+    if (menu.classList.contains("menu-show")) {
+      btn.setAttribute("aria-expanded", "true");
+      menuImg.src = "./assets/images/icon-menu-close.svg";
+    } else {
+      btn.setAttribute("aria-expanded", "false");
+      menuImg.src = "./assets/images/icon-menu.svg";
+    }
+  })
 })
+
